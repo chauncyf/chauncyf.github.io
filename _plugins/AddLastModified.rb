cf7269c03fb5a3e2fe5eb4340446526b4c1cdeff
chauncy <chauncyf@qq.com>
2020-04-09 19:07:12 -0400

    ..

--- _plugins/AddLastModified.rb
+++ _plugins/AddLastModified.rb
@@ -3,4 +3,8 @@ Jekyll::Hooks.register :posts, :pre_render do |post|
   modification_time = File.mtime(post.path)
   # inject modification_time in post's data
   post.data['last_modified'] = modification_time
+
+  if post.data['category'] == 'private'
+    post.data['sitemap'] = false
+  end
 end
