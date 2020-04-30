5146a05bb2a0387012dd17fff02d5ae016d1b9ff
chauncy <chauncyf@qq.com>
2020-04-30 14:21:40 -0400

    partly fix scrollspy slideUp

--- assets/js/generate_nav.js
+++ assets/js/generate_nav.js
@@ -2,25 +2,27 @@ $.fn.generateNav = function () {
     let res = ``;
     let preTag;
 
-    $("h2, h3").each(function () {
+    $('.navbar-brand').prop('href', `#${$('h1').prop('id')}`);
+
+    $('h2, h3').each(function () {
         let curTag = $(this);
 
-        if (curTag.prop("tagName") === 'H2') {
-            if (preTag && preTag.prop("tagName") === 'H3') {
+        if (curTag.prop('tagName') === 'H2') {
+            if (preTag && preTag.prop('tagName') === 'H3') {
                 res += `</nav>`;
             }
-            res += `<a class="nav-link nav-link-parent mb-1" href="#${$(this).prop("id")}">${$(this).text()}</a>`;
-        } else if (curTag.prop("tagName") === 'H3') {
-            if (preTag && preTag.prop("tagName") === 'H2') {
+            res += `<a class="nav-link nav-link-parent mb-1" href="#${$(this).prop('id')}">${$(this).text()}</a>`;
+        } else if (curTag.prop('tagName') === 'H3') {
+            if (preTag && preTag.prop('tagName') === 'H2') {
                 res += `<nav class="nav nav-pills flex-column nav-pills-collapse">`;
             }
-            res += `<a class="nav-link ml-3 mb-1" href="#${$(this).prop("id")}">${$(this).text()}</a>`;
+            res += `<a class="nav-link ml-3 mb-1" href="#${$(this).prop('id')}">${$(this).text()}</a>`;
         }
 
         preTag = curTag;
     });
 
-    if (preTag && preTag.prop("tagName") === 'H3') {
+    if (preTag && preTag.prop('tagName') === 'H3') {
         res += `</nav>`;
     }
 
