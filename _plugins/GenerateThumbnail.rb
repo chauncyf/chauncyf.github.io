01f335b20383ece731abfc91ba4afcef0062194c
Chauncy <chauncyff@gmail.com>
2020-05-25 02:37:06 -0400

    using svg

--- _plugins/GenerateThumbnail.rb
+++ _plugins/GenerateThumbnail.rb
@@ -9,7 +9,7 @@ Jekyll::Hooks.register :site, :post_read do |site|
   data_file_path = File.join('.', '_data', 'gallery.json')
 
   unless File.exist?(data_file_path)
-    puts 'Hooks: generating dominant color thumbnails..'
+    puts 'Hooks: finding image dominant color..'
 
     gallery_path = File.join('assets', 'gallery')
     gallery_data = []
@@ -24,15 +24,15 @@ Jekyll::Hooks.register :site, :post_read do |site|
       img_aspect_ratio = img_dimensions.map { |dimension| dimension / img_dimensions[0].gcd(img_dimensions[1]) }
       img_dominant_color = Miro::DominantColors.new(img_path).to_rgb[0]
 
-      img_thumbnail = ChunkyPNG::Image.new(img_aspect_ratio[0], img_aspect_ratio[1], ChunkyPNG::Color::TRANSPARENT)
-
-      (0...img_aspect_ratio[0]).each do |row|
-        (0...img_aspect_ratio[1]).each do |col|
-          img_thumbnail[row, col] = ChunkyPNG::Color.rgb(img_dominant_color[0], img_dominant_color[1], img_dominant_color[2])
-        end
-      end
-
-      img_base64 = Base64.strict_encode64(img_thumbnail.to_datastream.to_blob)
+      # img_thumbnail = ChunkyPNG::Image.new(img_aspect_ratio[0], img_aspect_ratio[1], ChunkyPNG::Color::TRANSPARENT)
+      #
+      # (0...img_aspect_ratio[0]).each do |row|
+      #   (0...img_aspect_ratio[1]).each do |col|
+      #     img_thumbnail[row, col] = ChunkyPNG::Color.rgb(img_dominant_color[0], img_dominant_color[1], img_dominant_color[2])
+      #   end
+      # end
+      #
+      # img_base64 = Base64.strict_encode64(img_thumbnail.to_datastream.to_blob)
 
       gallery_data.push({
                             'name': img,
@@ -40,7 +40,8 @@ Jekyll::Hooks.register :site, :post_read do |site|
                             'path': File.join('', gallery_path, img),
                             'width': img_dimensions[0],
                             'height': img_dimensions[1],
-                            'base64': img_base64_prefix + img_base64
+                            'dominant_color': img_dominant_color.join(','),
+                            # 'base64': img_base64_prefix + img_base64
                         })
     end
 
